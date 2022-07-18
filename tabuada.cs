de84341c99a3a88cee4ddd8dd43d574f2be9ee24
VictorCosta2002 <victorcosta123vcs@gmail.com>
2022-07-18 10:38:10 -0300

    Tabuada

---
+++ tabuada.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static public int Tabu = 0;
+        static public int Produto = 0; 
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Olá digite o numero que voce deseja saber a tabuada: \n");
+            Tabu = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("o resultado deu:");
+
+            for (int i = 0; i <=10; i++)
+            {
+                Produto = i * Tabu;
+                
+                Console.WriteLine(Tabu +" X "+ i + "= "+ Produto);
+
+            }
+
+        }
+    }
+}
